6a03cd08fd6b461a21ac66d2408ffbf1ab13f857
Your Name <your-email@example.com>
2025-08-11 20:38:37 +0700

    first commit

---
+++ js-mini-todo-app/main.js
@@ -0,0 +1,42 @@
+const tasks = [];
+
+const taskList = document.querySelector(`#task-list`);
+const todoForm = document.querySelector(`#todo-form`);
+const todoInput = document.querySelector(`#todo-input`);
+
+todoForm.onsubmit = function (e) {
+    e.preventDefault();
+    const value = todoInput.value.trim();
+    if (!value) {
+        return alert("please enter something");
+    }
+
+    const newTask = {
+        title: value,
+        completed: false,
+    };
+    tasks.push(newTask);
+    render();
+};
+
+function render() {
+    const html = tasks
+        .map(
+            (task) =>
+                `<li class="task-item ${task.completed ? "completed" : ""}">
+        <span class="task-title">${task.title}</span>
+            <div class="task-action">
+                <button class="task-btn edit">Edit</button>
+                <button class="task-btn done">${
+                    task.completed ? "Mark as undone" : "Mark as done"
+                }</button>
+                <button class="task-btn delete">Delete</button>
+            </div>
+        </li>`
+        )
+        .join("");
+
+    taskList.innerHTML = html;
+}
+
+render();
