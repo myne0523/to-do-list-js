68d60f1242eb47a4c9bd2e633fb731fa8ccde393
Your Name <your-email@example.com>
2025-08-12 11:58:24 +0700

    Chỉnh sửa công việc

--- js-mini-todo-app/main.js
+++ js-mini-todo-app/main.js
@@ -4,6 +4,22 @@ const taskList = document.querySelector(`#task-list`);
 const todoForm = document.querySelector(`#todo-form`);
 const todoInput = document.querySelector(`#todo-input`);
 
+taskList.onclick = function (e) {
+    const taskItem = e.target.closest(".task-item");
+    const taskIndex = +taskItem.getAttribute("task-index");
+    const task = tasks[taskIndex];
+
+    if (e.target.closest(".edit")) {
+        const newTitle = prompt("Enter the new task title", task.title);
+        task.title = newTitle;
+        render();
+    } else if (e.target.closest(".done")) {
+        console.log("Mark as done/undone");
+    } else if (e.target.closest(".delete")) {
+        console.log("Delete");
+    }
+};
+
 todoForm.onsubmit = function (e) {
     e.preventDefault();
     const value = todoInput.value.trim();
@@ -16,23 +32,30 @@ todoForm.onsubmit = function (e) {
         completed: false,
     };
     tasks.push(newTask);
+
+    // re-render
     render();
+
+    // clear input
+    todoInput.value = "";
 };
 
 function render() {
     const html = tasks
         .map(
-            (task) =>
-                `<li class="task-item ${task.completed ? "completed" : ""}">
-        <span class="task-title">${task.title}</span>
-            <div class="task-action">
-                <button class="task-btn edit">Edit</button>
-                <button class="task-btn done">${
-                    task.completed ? "Mark as undone" : "Mark as done"
-                }</button>
-                <button class="task-btn delete">Delete</button>
-            </div>
-        </li>`
+            (task, index) =>
+                `<li class="task-item ${
+                    task.completed ? "completed" : ""
+                }" task-index= "${index}">
+                    <span class="task-title">${task.title}</span>
+                    <div class="task-action">
+                        <button class="task-btn edit">Edit</button>
+                        <button class="task-btn done">${
+                            task.completed ? "Mark as undone" : "Mark as done"
+                        }       </button>
+                        <button class="task-btn delete">Delete</button>
+                    </div>
+                </li>`
         )
         .join("");
 
